5d15433362d48ff426fde46a1f177c9ffdbc8d21
typeluis <typeluis@gmail.com>
2022-08-27 16:23:09 -0400

    adding questions

--- Test interview/Answers.js
+++ Test interview/Answers.js
@@ -21,3 +21,32 @@ function Question1(s, t){
 
 
 console.log(Question1('ab12c', 'ab24z'))
+
+
+const Question2 = (a) => {
+    for(i=0; i < a.length; i++){
+        const b = a[i]
+
+        for(j=0; j <b.length ; j++){
+            const currentNum = b[j]
+            if(i != a.length -1 && typeof a[i+1][j-1] != 'undefined'){
+                const bottomDiagnalNum = a[i+1][j-1]
+                if (currentNum < bottomDiagnalNum){
+                    b[j] = currentNum
+                    a[i+1][j-1] = bottomDiagnalNum
+                }
+            }
+
+            else if (i != 0 && typeof a[i-1][j+1] != 'undefined'){
+                const topDiagnalNum = a[i-1][j+1]
+
+                if (currentNum > topDiagnalNum){
+                    b[j] = topDiagnalNum
+                    a[i - 1][j+1] = currentNum
+                }
+            }
+        }
+    }
+
+    return a
+}
